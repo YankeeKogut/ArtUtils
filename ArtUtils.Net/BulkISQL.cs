f1a1794aa11fa5a719ccea08a66e741b133fdbc4
YankeeKogut <artsikhovsky@gmail.com>
2019-06-07 15:11:50 -0400

    new overload

--- ArtUtils.Net/BulkISQL.cs
+++ ArtUtils.Net/BulkISQL.cs
@@ -1,21 +1,35 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace ArtUtils.Net
 {
     public static class BulkSql
     {
-        public static void Insert(DataTable dataTable, string sqlConnectionString, string targetTableName, string targetSchemaName)
-        { 
+        public static void Insert(DataTable dataTable, string sqlConnectionString, string targetTableName, string targetSchemaName = "")
+        {
             var connection = new SqlConnection(sqlConnectionString);
-            
+
             Insert(dataTable, connection, targetTableName, targetSchemaName);
         }
 
-        public static void Insert(DataTable dataTable, SqlConnection sqlConnection, string targetTableName, string targetSchemaName="")
+        public static void Insert(DataTable dataTable, SqlConnection sqlConnection)
         {
-            var  dest =targetSchemaName == string.Empty 
-                ? targetTableName 
+            var tableName = dataTable.TableName;
+            if (string.IsNullOrEmpty(tableName))
+            {
+                throw new ArgumentException("DataTable must have TableName property populated, Namespace property is optional.");
+            }
+
+            var schema = dataTable.Namespace;
+
+            Insert(dataTable, sqlConnection, tableName, schema);
+        }
+
+        public static void Insert(DataTable dataTable, SqlConnection sqlConnection, string targetTableName, string targetSchemaName = "")
+        {
+            var dest = targetSchemaName == string.Empty
+                ? targetTableName
                 : targetSchemaName + "." + targetTableName;
 
             var bulkCopy =
