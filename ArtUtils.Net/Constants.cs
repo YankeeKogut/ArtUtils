7d7f8a395d9e24f79fcd97e9b21f72d3a6e36cd8
YankeeKogut <artsikhovsky@gmail.com>
2019-06-13 08:20:33 -0400

    nuget changes

--- ArtUtils.Net/Constants.cs
+++ ArtUtils.Net/Constants.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArtUtils.Net
+﻿namespace ArtUtils.Net
 {
-    public class Constants
+    public static class Constants
     {
         public const string ErrWrongDataType =
             "The given value of type String from the data source cannot be converted to type int of the specified target column";
---
+++ ArtUtils.Net/build.nuget.bat
@@ -0,0 +1,1 @@
+nuget pack ArtUtils.Net.csproj
